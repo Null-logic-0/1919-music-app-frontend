9766ce1af42e0dbdbfe7c5d35d6d635e7eaa4017
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-07-04 17:49:39 +0400

    feat:created heart shape like button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeartLike from "./Components/HeartLike/HeartLike";
 
 export default function Home() {
   return (
     <main>
+      {/*<HeartLike/>*/}
       
     </main>
   );
