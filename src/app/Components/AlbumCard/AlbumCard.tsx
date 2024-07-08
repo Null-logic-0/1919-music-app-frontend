bc023050bb6e36d7428047cc14a970f2671ba174
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-07-08 21:18:36 +0400

    refactor:made link optional for albumCards

--- src/app/Components/AlbumCard/AlbumCard.tsx
+++ src/app/Components/AlbumCard/AlbumCard.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 import styles from './AlbumCard.module.scss';
 import AlbumImage from './AlbumImage';
 import AlbumTitle from './AlbumTitle';
-import AlbumSubtitle from './AlbumSubtitle'; 
+import AlbumSubtitle from './AlbumSubtitle';
 import { ImageSizeVariant } from '../../enums/imageSizeVariants';
+import Link from 'next/link';
 
 type AlbumProps = {
     showDetails?: boolean;
@@ -12,10 +13,18 @@ type AlbumProps = {
     direction?: 'row' | 'column';
     imageSizeVariant?: ImageSizeVariant;
     images: string;
-    
+    link?: string;
 };
 
-const AlbumCard = ({ showDetails, title, subtitle, direction, imageSizeVariant = ImageSizeVariant.Medium, images }: AlbumProps) => {
+const AlbumCard = ({
+    showDetails,
+    title,
+    subtitle,
+    direction,
+    imageSizeVariant = ImageSizeVariant.Medium,
+    images,
+    link, 
+}: AlbumProps) => {
     const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
@@ -35,31 +44,32 @@ const AlbumCard = ({ showDetails, title, subtitle, direction, imageSizeVariant =
     if (direction === 'row') classNames.push(styles.row);
     else classNames.push(styles.column);
 
-
     const classes = [styles.info];
-    if (imageSizeVariant === ImageSizeVariant.XLarge ) {
-        classes.push(styles.info); 
+    if (imageSizeVariant === ImageSizeVariant.XLarge) {
+        classes.push(styles.info);
     } else if (direction === 'row') {
         classes.push(styles.columnInfo);
-    } else if (imageSizeVariant === ImageSizeVariant.RoundedXXL || imageSizeVariant === ImageSizeVariant.RoundedXL) {
+    } else if (
+        imageSizeVariant === ImageSizeVariant.RoundedXXL ||
+        imageSizeVariant === ImageSizeVariant.RoundedXL
+    ) {
         classes.push(styles.roundedColumn);
-    } 
-
-    
-    
+    }
 
     return (
         <div className={classNames.join(' ').trim()}>
-            <AlbumImage
-                src={images}
-                alt='Album Cover'
-                imageSizeVariant={imageSizeVariant}
-            />
+            {link ? (
+                <Link href={link || '#'}>
+                    <AlbumImage src={images} alt="Album Cover" imageSizeVariant={imageSizeVariant} />
+                </Link>
+            ) : (
+                <AlbumImage src={images} alt="Album Cover" imageSizeVariant={imageSizeVariant} />
+            )}
 
             {showDetails && (
                 <div className={`${classes.join(' ').trim()} `}>
-                    <AlbumTitle title={title} imageSizeVariant={imageSizeVariant} />
-                    <AlbumSubtitle subtitle={subtitle} imageSizeVariant={imageSizeVariant} />
+                    {title && <AlbumTitle title={title} imageSizeVariant={imageSizeVariant} />}
+                    {subtitle && <AlbumSubtitle subtitle={subtitle} imageSizeVariant={imageSizeVariant} />}
                 </div>
             )}
         </div>
