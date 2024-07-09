9bc26dad0fb622e30e1d4ceb1bf1c311507124f9
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-07-09 22:15:46 +0400

    fix: removed Player component from the home page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { ImageSizeVariant } from "./enums/imageSizeVariants";
-import PlayerController from "./Components/PlayerControler/PlayerControler";
 
 
 export default function Home() {
   return (
     <main>
-      <PlayerController/>
 
-    
     </main>
   );
 }
