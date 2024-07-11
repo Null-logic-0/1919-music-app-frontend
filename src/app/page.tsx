4f25fd43de896c7bba5ee745a90b4b4518324bfb
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-07-11 16:13:09 +0400

    feat: Created search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
-import { ImageSizeVariant } from "./enums/imageSizeVariants";
-import AddButton from "./Components/AddButton/AddButton";
+
 
 
 export default function Home() {
+  
   return (
     <main>
-      
-    
     </main>
   );
 }
