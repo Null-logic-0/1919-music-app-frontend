c3f657850f3c7a7a2ebaed080bf3b4dcd7bd8c8d
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-07-20 17:02:11 +0400

    feat:created play Add and delete buttons for music table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import DayWeekDRP from "./Components/dropDown/DayWeekDropContent/DayWeek";
 import NavMenu from "./Components/NavMenu/NavMenu";
 import ResponsiveMenu from "./Components/NavMenu/ResponsiveMenu/ResponsiveMenu";
 import PlayListFrom from "./Components/PlayListFrom/PlayListFrom";
+import MultiTaskButton from "./Components/MultiTaskButton/MultiTaskButton";
 
 
 export default function Home() {
