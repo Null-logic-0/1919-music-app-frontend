50ad803d24412621970f494bba2b7c73f38dcfda
giorgimuktiashvili <giorgimuqtiashvili@gmail.com>
2024-07-24 12:58:11 +0400

    Feat:Created Top Page

--- src/app/(authorised)/topalbum/page.tsx
+++ src/app/(authorised)/topalbum/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
-import TopAlbum from "./Components/TopAlbum/TopAlbum";
+import TopAlbum from "@/app/Components/TopAlbum/TopAlbum";
+
+
 
 export default function Home() {
 
   return (
     <main>      
+      <TopAlbum />
     </main>
   )
 }
