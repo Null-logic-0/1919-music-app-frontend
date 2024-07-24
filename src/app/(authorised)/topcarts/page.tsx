bba4a5c285ea368dbd33da7399ec37cfe2503783
giorgimuktiashvili <giorgimuqtiashvili@gmail.com>
2024-07-24 13:12:32 +0400

    Feat:Top Carts Page

--- src/app/(authorised)/topcarts/page.tsx
+++ src/app/(authorised)/topcarts/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import TopCarts from "@/app/Components/TopCarts/TopCarts";
 
 
 export default function Home() {
 
   return (
     <main>      
+        <TopCarts/>
     </main>
   )
 }
---
+++ src/app/Components/TopCarts/TopCarts.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import AlbumCard from "../AlbumCard/Card";
+import styles from './TopCarts.module.scss';
+import CardSubtitle from "../AlbumCard/CardSubtitle/CardSubtitle";
+import CardTitle from "../AlbumCard/CardTitle/CardTitle";
+import { ImageSizeVariant} from "../../enums/imageSizeVariants";
+import Card from "../AlbumCard/Card";
+import Heading from "../Heading/Heading";
+
+
+ const AlbumData = [
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'},
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'},
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'},
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'},
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'},
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'},
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'},
+  { title:'Song Name', image: "/images/albumCard.png",subtitle:'100 song'}, 
+];
+
+
+
+
+
+const TopCarts = () => {
+  const [currentAlbums] = useState(AlbumData);
+
+  return (
+  
+    <div className={styles.container}>
+        <Heading title="Top Chrts" />
+      <div className={styles.albumWrapper}>
+        {currentAlbums.map((item,index) => (   
+        <Card 
+         key={index}
+         images={item.image} 
+         title={item.title} subtitle={item.subtitle} 
+         showDetails  
+         direction="column" 
+         imageSizeVariant={ImageSizeVariant.XLarge}
+         />          
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopCarts;
