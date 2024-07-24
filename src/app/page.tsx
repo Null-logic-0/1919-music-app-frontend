4a1cab7c4b419a4bdc4b184558c59420e87c5e27
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-07-24 11:52:23 +0400

    feat:created header top for tablet and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeaderTop from "./Components/HeaderLogo/HeaderTop";
 
 
 export default function Home() {
 
   return (
-    <main>      
+    <main> 
     </main>
+  )
 }
