cf24e091d55d1bad381f484a2a5906a33c32e687
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-07-30 21:08:34 +0400

    fix:added import for logout

--- src/app/Components/HomePageTop/HomePageTop.tsx
+++ src/app/Components/HomePageTop/HomePageTop.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Search from '../Search/Search';
 import styles from './HomePageTop.module.scss';
 import MultiTaskButton from '../MultiTaskButton/MultiTaskButton';
+import Logout from '../LogOut/LogOut';
 
 const HomePageTop = () => {
     const [searchTerm, setSearchTerm] = useState('');
