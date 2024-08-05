81cf78943e5e3a2b27e1fd787c5c8188887bc617
giorgimuktiashvili <giorgimuqtiashvili@gmail.com>
2024-08-05 13:48:47 +0400

    feat: created one album page

---
+++ src/app/(authorised)/topalbum/[id]/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Image from "next/image";
+import styles from "./page.module.scss";
+import OneAlbum from "@/app/Components/OneAlbum/OneAlbum";
+
+
+export default function Home() {
+
+  return (
+    <main className={styles.main}>
+        <OneAlbum />
+    </main>
+  )
+}
