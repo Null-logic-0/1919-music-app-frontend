c1a664c88430699b7530ac3bf0aff756f063f07b
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-09-14 17:25:19 +0400

    refactor:top-10 songs changed default by week

--- src/app/Components/HomePageTop/HomePageTop.tsx
+++ src/app/Components/HomePageTop/HomePageTop.tsx
@@ -1,12 +1,8 @@
 'use client'
-import { useState } from 'react';
-import Search from '../Search/Search';
 import styles from './HomePageTop.module.scss';
-import MultiTaskButton from '../MultiTaskButton/MultiTaskButton';
 import Logout from '../LogOut/LogOut';
 
 const HomePageTop = () => {
-    const [searchTerm, setSearchTerm] = useState('');
     return (
         <div className={styles.container}>
             <Logout/>
