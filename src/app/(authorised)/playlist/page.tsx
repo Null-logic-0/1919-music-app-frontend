b8afc5444af92a4b3812351d803bd3ca8f371414
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-09-17 09:52:58 +0400

    fixed:authguard fix

--- src/app/(authorised)/playlist/page.tsx
+++ src/app/(authorised)/playlist/page.tsx
@@ -1,17 +1,15 @@
-import Playlists from '@/app/Components/Playlists/Playlists';
-import styles from './page.module.scss';
-import PagesHeaderTop from '@/app/Components/PagesHeaderTop/PagesHeaderTop';
+import Playlists from "@/app/Components/Playlists/Playlists";
+import styles from "./page.module.scss";
+import PagesHeaderTop from "@/app/Components/PagesHeaderTop/PagesHeaderTop";
 
 const PlaylistPage = () => {
-    return (
-        <div className={styles.main}>
-            <PagesHeaderTop link={'/'} />
+  return (
+    <div className={styles.main}>
+      <PagesHeaderTop link={"/"} />
 
-            <Playlists />
-
-
-        </div>
-    )
-}
+      <Playlists />
+    </div>
+  );
+};
 
 export default PlaylistPage;
